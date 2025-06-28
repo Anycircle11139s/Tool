82288f6c4c7f3fdf5ab0f54f48aaccf3b77f027b
Anycircle11139s <q35481193@gmail.com>
2025-06-28 11:35:27 +1000

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,14 +1,30 @@
-const CACHE = 'toolkit-v1';
-const ASSETS = ['index.html','style.css','script.js'];
+const CACHE_NAME = 'browser-toolkit-cache-v1';
+const FILES_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/script.js',
+  '/favicon.ico'
+];
 
-self.addEventListener('install',e=>e.waitUntil(
-  caches.open(CACHE).then(c=>c.addAll(ASSETS))
-));
-
-self.addEventListener('fetch',e=>{
-  if(e.request.method!=='GET')return;
-  e.respondWith(
-    caches.match(e.request).then(r=>r||fetch(e.request))
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
   );
 });
 
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+      )
+    )
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(resp => resp || fetch(e.request))
+  );
+});
